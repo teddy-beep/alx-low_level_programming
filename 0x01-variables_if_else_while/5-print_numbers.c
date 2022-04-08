c0387a20c4aa3c70d453034affc9e03ffc15ec2f
Tewodros <teddymekuria277@gmail.com>
2022-04-08 14:42:04 -0700

    all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main – Entry point
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
