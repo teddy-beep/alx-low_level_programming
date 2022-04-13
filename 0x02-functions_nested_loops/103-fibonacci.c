4ebf87996814aa80d8e876cb4bb1d3ed07f0cd26
Tewodros <teddymekuria277@gmail.com>
2022-04-13 03:03:07 -0700

    prints the sum of the even-valued terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,18 +6,19 @@
  */
 int main(void)
 {
-unsigned long count, i = 0, j = 1, k, sums = 0;
-
+unsigned long count, i, j, k, sums;
+i = sums = 0;
+j = 1;
 for (count = 0; count < 50; count++)
 {
 k = i + j;
 i = j;
 j = k;
-if(k % 2 == 0 && sums < 4000000)
+if(k % 2 == 0 && k < 4000000)
 {
 	sums += k;
 }
 }
-printf("%lu\n, sums");
+printf("%lu\n", sums);
 return (0);
 }
