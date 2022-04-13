3d4413529246b922a10a56ce97e3de6653a87540
Tewodros <teddymekuria277@gmail.com>
2022-04-13 05:33:07 -0700

    all task is done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,9 +11,10 @@ int count;
 unsigned long i, j, k;
 unsigned long m, n, p, carry;
 
+count = 0;
 i = 0;
 j = 1;
-for (count = 0; count <= 91; count++)
+for (count = 1; count < 92; count++)
 {
 k = i + j;
 i = j;
