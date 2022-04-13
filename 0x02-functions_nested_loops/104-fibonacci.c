b2dca9f848d3d4d6737a632ea6f0bae234330bd8
Tewodros <teddymekuria277@gmail.com>
2022-04-13 03:19:21 -0700

    All task is done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,37 +8,39 @@
 int main (void)
 {
 int count;
-unsigned long fib1 = 0, fib2 = 1, sum;
-unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-unsigned long half1, half2;
-for (count = 0; count < 92; count ++)
+unsigned long i, j, k;
+unsigned long m, n, p, caryy;
+
+count 0;
+i = 0;
+j = 1;
+for (count = 1; count <= 91; count ++)
 {
-sum = fib1 + fib2;
-printf("%lu,", sum);
-fib1 = fib2;
-fib2 = sum;
-fib1_half1 = fib1 / 10000000000
-fib2_half1 = fib2 / 10000000000
-fib1_half2 = fib1 % 10000000000
-fib2_half2 = fib2 % 10000000000
-for (count = 93; count < 99; count++)
-{
-half1 = fib1_half1 + fib2_half1;
-half2 = fib1_half2 + fib2_half2;
-if (fib1_half2 + fib2_half2 > 9999999999)
-{
-half1 += 1;
-half2 %= 10000000000;
-}
-print f("%lu%lu", half1, half2);
+k = i + j;
+i = j;
+j = k;
+printf("%lu, ", k);
+n = i % 1000;
+i = i / 1000;
+n = j % 1000;
+j = j / 1000;
+while (count <= 98)
+
+carry = (m + n) / 1000;
+p = (m + n) - carry * 1000;
+k = (i + j) + carry;
+m = n;
+n = p;
+i = j;
+j = k;
+if (p >= 100)
+	printf("%lu%lu", k, p);
+else
+	printf("%lu0%lu", k, p);
 if (count != 98)
-printf(",");
-fib1_half1 = fib2_half1;
-fib1_half2 = fib2_half2;
-fib2_half1 = half1;
-fib2_half2 = half2;
+	printf(",");
+count++;
 }
-printf("\n");
+putchar("\n");
 return (0);
 }
-
