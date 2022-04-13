4eecb7d65eca57122f8243c433a1b802aad63cd7
Tewodros <teddymekuria277@gmail.com>
2022-04-13 02:35:28 -0700

    task 12 is done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ _putchar((prod % 10) + '0');
 else if (prod >= 100 && mult != 0)
 {
 _putchar((prod / 100) + '0');
-_putchar(((prod / 10)) % 10 + '0');
+_putchar((prod / 10) % 10 + '0');
 _putchar((prod % 10) + '0');
 }
 else
