8c2dab9ab486ea60351bf52d8c6410ba95f4db50
Tewodros <teddymekuria277@gmail.com>
2022-04-14 03:05:22 -0700

    multipies two intigers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Multiply two intigers
+ * @a: The first integer to be added
+ * @b: The second integer to be added
+ *
+ * Return: The result of the addition.
+ */
+int mul(int a, int b)
+{
+return (a + b);
+}
