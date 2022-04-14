ff7a5fd1f530276664385106c7766e2dd16256ba
Tewodros <teddymekuria277@gmail.com>
2022-04-14 02:44:05 -0700

    check uppercase letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower -Checks if a character is lowercase.
+ * _isupper - Checks if a character is uppercase.
  * @c: The character to be checked.
  *
  * Return: 1 if character is lowercase, 0 otherwise.
