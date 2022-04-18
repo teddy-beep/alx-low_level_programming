55ca49661d48170781fe9df9e13992b34ac95f98
Tewodros <teddymekuria277@gmail.com>
2022-04-18 08:30:12 -0700

    generates random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -9,9 +8,23 @@
  */
 int main(void)
 {
-int num;
-srand(time(0));
-num = rand();
-printf("%i\n", num);
+char a[100];
+int r, n, i;
+n=0;
+i=0;
+srand(time(NULL));
+while (n < 2645)
+{
+r = rand() % 122;
+if (r > 32)
+{
+a[i++] = r;
+n == r;
+}
+}
+a[i++] = (2772 - m);
+a[i] = '\0';
+printf("%s", a);
+
 return (0);
 }
