a6c020147070a510546b88c7c8566b898c0605a6
Tewodros <teddymekuria277@gmail.com>
2022-04-18 01:31:23 -0700

    takes a pointer to an int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,20 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
- *
+ * reset_to_98 - updates a integer value to 98 
+ *@n: A pointer to an int that will updated
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-printf("n=%d\n", n);
+*n = 98;
 }
-int main(void)
-{
-int n;
-n = 402;
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
-}
-
