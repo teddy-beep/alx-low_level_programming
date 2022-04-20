2642b03979bce72276b096080cac1bca3163da90
Tewodros <teddymekuria277@gmail.com>
2022-04-20 09:49:21 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 i = 0;
-while (dest [i] != '\0')
+while (dest[i] != '\0')
 {
 i++;
 }
@@ -20,6 +20,6 @@ dest[i] = src[j];
 j++;
 i++;
 }
-dest [i] = '\0';
+dest[i] = '\0';
 return (dest);
 }
