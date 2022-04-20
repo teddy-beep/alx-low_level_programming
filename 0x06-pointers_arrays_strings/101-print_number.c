a79d8ca00f88817b704729a903e991f2725d6ac3
Tewodros <teddymekuria277@gmail.com>
2022-04-20 12:14:23 -0700

    a function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,25 +7,25 @@
 
 void print_number(int n)
 {
-  unsigned int i = 1;
+unsigned int i = 1;
 
-  if (n < 0)
-  {
-    _putchar('-');
-    n *= -1;
-  }
-  if (n == 0)
-    _putchar('0');
-  else
-  {
-    while ((n / i) >= 10)
-      i *= 10;
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+if (n == 0)
+_putchar('0');
+else
+{
+while ((n / i) >= 10)
+i *= 10;
 
-    while (i > 0)
-    {
-      _putchar((n / i) + '0');
-      n %= i;
-      i /= 10;
-    }
-  }
+while (i > 0)
+{
+_putchar((n / i) + '0');
+n %= i;
+i /= 10;
+}
+}
 }
