2414b51bc5c8cb96da223ff3dad92482b44b4aab
Tewodros <teddymekuria277@gmail.com>
2022-05-19 03:55:56 -0700

    First task is done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,16 +6,16 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t nelem;
-nelem = 0;
-while (h != NULL)
+size_t i;
+i = 0;
+while (h)
 {
-if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
+if (!h->str)
+printf("[0] (nil)\n");
 else
-printf("[%d] %s\n", h->len, h->str);
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
-nelem++;
+i++;
 }
-return (nelem);
+return (i);
 }
