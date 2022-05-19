9807c28f59be1e191d4000b0c7f303214156f812
Tewodros <teddymekuria277@gmail.com>
2022-05-19 13:19:56 -0700

    first task is done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,7 @@
-#include <unistd.h>
-#include "lists.h"
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <lists.h>
 /**
  * print_list - print counter all elements of `list_t` list
  * @h: head of linked list
