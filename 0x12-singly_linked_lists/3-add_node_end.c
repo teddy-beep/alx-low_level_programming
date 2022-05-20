0912df3a5383e7b1cc8f444af808cde5bd0d1502
Tewodros <teddymekuria277@gmail.com>
2022-05-20 11:27:19 -0700

    The forth task is done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,41 @@
 #include "lists.h"
-/**
- * add_node_end - Add a new node at the end of a list.
- * @head: Address of the first node of a list.
- * @str: Address of the string to insert into the new node.
- * Return: Address of the new node.
- **/
+#include <string.h>
 
+/**
+ * add_node_end - adds a node at the end, my dude
+ * @head: points to the head
+ * @str: the string to put into it
+ * Return: pointer to new node or NULL
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *i, *j;
-	unsigned int length = 0;
+	list_t *newNode, *last;
+	unsigned int i;
 
-	if (str == NULL)
+	if (!(head && str))
 		return (NULL);
 
-	i = malloc(sizeof(list_t));
-	if (i == NULL)
+	newNode = malloc(sizeof(list_t));
+	if (!newNode)
 		return (NULL);
 
-	i->str = strdup(str);
-	if (i->str == NULL)
+	newNode->str = strdup(str);
+	for (i = 0; str[i]; i++)
+	{}
+	newNode->len = i;
+	newNode->next = NULL;
+
+	if (!*head)
 	{
-		free(i);
-		return (NULL);
-	}
-	while (str[length])
-		length++;
-	i->len = length;
-	i->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = i;
-		return (i);
+		*head = newNode;
+		return (newNode);
 	}
 
-	j = *head;
-	while (j->next)
-		j = j->next;
-	j->next = i;
-	return (i);
+	last = *head;
+	while (last->next)
+	{
+		last = last->next;
+	}
+	last->next = newNode;
+	return (newNode);
 }
