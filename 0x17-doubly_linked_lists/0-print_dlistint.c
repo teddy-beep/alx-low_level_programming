0f4abbe1d53a56d8524a3c02bcff0debdab6a203
Tewodros <teddymekuria277@gmail.com>
2022-07-07 10:16:03 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,16 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t con = 0;
+
 	if (h == NULL)
 		return (0);
+
 	while (h)
 	{
 		printf("%d\n", h->n);
 		con++;
 		h = h->next;
 	}
+
 	return (con);
 }
